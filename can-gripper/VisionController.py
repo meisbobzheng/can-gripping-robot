39eb1a18fb1a6089e00615a92dc9c652090685b0
Ari Pant <pantari76@gmail.com>
2025-04-07 15:04:54 -0400

    add vision function to store confidence scores

--- can-gripper/VisionController.py
+++ can-gripper/VisionController.py
@@ -12,16 +12,41 @@ class VisionController:
         self.cap = cv2.VideoCapture(1)
         self.processor = Owlv2Processor.from_pretrained("google/owlv2-base-patch16-ensemble")
         self.model = Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble")
+        # list of tuples (confidence_score of the can, octet number)
+        self.cans_observed = []
 
 
-    def get_can_position(self):
+    def get_can_position(self, can_description, octet_number):
         """
-        Scan around using movement controller, then identity can and return the estimated position. 
+        NOTE: This has not been tested
+        
+        can_description: str - The can that we are searching for ("a green can of sprite") 
+        octet_number: int    - The octet number that describes the section of the field that the camera is looking at
+
+        We are assuming that the camera is looking at 1/8th of the current field.
+        This function will store the confidence scores of all the cans in the field in the cans_observed function.
         """
-        pass
+        for _ in range(100):
+            _, frame = self.cap.read()
+
+        texts = [[can_description]]
+        inputs = self.processor(text=texts, images=frame, return_tensors="pt")
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        results = self.processor.post_process_object_detection(outputs=outputs, target_sizes=torch.Tensor([(640, 480)]), threshold=0.2)
+        for s in results[0]["scores"]:
+            self.cans_observed.append((s, octet_number))
+        
+        return True
+
+
 
     def center_the_frame(self, can_description):
         """
+        can_description: str - The can that we are searching for ("a green can of sprite") 
+
         Will return the percentage to tilt the camera either right or left such that the can is centered in the frame.
         This is assuming that the camera is horizontal to the table.
         """
