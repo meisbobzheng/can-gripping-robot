5365bacbe8e54088ef4facd1c6d51ddbac1f0ec7
rover <iankaish23@gmail.com>
2025-04-11 14:58:49 -0400

    Some stuff works better 6 dof

---
+++ movement/moveNew.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
+from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
+import numpy as np
+
+"""
+This script makes the end-effector go to a specific pose by defining the pose components
+
+To get started, open a terminal and type:
+
+    ros2 launch interbotix_xsarm_control xsarm_control.launch robot_model:=wx250
+
+Then change to this directory and type:
+
+    python3 ee_pose_components.py
+"""
+
+
+# calculates the position the arm should go to before moving forward to grab can
+def calculate_first_pos(can_x, can_y):
+    # robot origin is (0,0,~.2)
+
+    # y = mx + b
+
+    # (0,0)
+    # m = can_y / can_x
+    # b = 0
+    # y = can_y / can_x
+
+    # percentage of x to move to (Can be adjusted if we start hitting cans)
+    percent_x = 0.75
+    percent_x_pos = percent_x * can_x
+
+    pos_y = (can_y / can_x) * percent_x_pos + 0
+    print("HELLOOOO")
+    print(percent_x_pos)
+    print(pos_y)
+    return percent_x_pos, pos_y
+
+
+def new_scout_pos():
+    bot.arm.go_to_sleep_pose()
+    bot.arm.set_ee_pose_components(x=0.27, z=0.07)
+
+    # bot.arm.set_single_joint_position(joint_name="waist", position=np.pi / 2.0)
+    # bot.gripper.release()name="waist", position=np.pi / 2.0)
+    # bot.gripper.release()
+
+
+def new_extend(distance):
+    # roughly .1 to .4 is pretty good we can test
+    bot.arm.set_ee_cartesian_trajectory(x=distance)
+
+
+def scout_pos():
+    # adjust shoulder and elbow to move end effector straight down
+    shoulder_index = bot.arm.info_index_map["shoulder"]
+    elbow_index = bot.arm.info_index_map["elbow"]
+    wrist_rotate_index = bot.arm.info_index_map["wrist_rotate"]
+    wrist_angle_index = bot.arm.info_index_map["wrist_angle"]
+
+    # flatten wrist to be parallel
+    # wrist angle to look up at - maybe calculate, maybe can just cheat and hard code
+    wrist_rotate_angle = np.pi / 2
+    wrist_angle = -np.pi / 70
+    # - twists it right
+
+    bot.arm.go_to_sleep_pose()
+
+    scout_joints_from_sleep = bot.arm.get_joint_positions()
+    scout_joints_from_sleep[wrist_rotate_index] = wrist_rotate_angle
+    scout_joints_from_sleep[wrist_angle_index] = wrist_angle
+
+    # scout_joints_from_sleep[elbow_index] = math.pi / 2.6  # 2.5 is old
+    # scout_joints_from_sleep[shoulder_index] = 0  # 8 is old
+    print(bot.arm.info_index_map)
+    print(scout_joints_from_sleep)
+    bot.arm.set_joint_positions(scout_joints_from_sleep)
+    print("scouting")
+
+
+# Rotate waist by an angle (radian)
+# positive is left, negative is right
+def rotate_waist(angle):
+    bot.arm.set_single_joint_position("waist", angle)
+
+
+def extend_arm():
+    curr_joints = bot.arm.get_joint_positions()
+
+    old_joints = curr_joints
+
+    shoulder_index = bot.arm.info_index_map["shoulder"]
+    elbow_index = bot.arm.info_index_map["elbow"]
+
+    curr_joints[shoulder_index] = math.pi / 5
+    curr_joints[elbow_index] = math.pi / 4
+
+    bot.arm.set_joint_positions(curr_joints)
+
+    print("extending arm")
+
+
+if __name__ == "__main__":
+
+    bot = InterbotixManipulatorXS(
+        robot_model="wx250s",
+        group_name="arm",
+        gripper_name="gripper",
+    )
+
+    robot_startup()
+
+    # Go to scout pos
+    new_scout_pos()
+
+    # rotate_waist(-math.pi / 2)
+
+    # extend_arm()
+
+    robot_shutdown()
---
+++ movement/test_model.py
@@ -0,0 +1,75 @@
+from transformers import Owlv2Processor, Owlv2ForObjectDetection
+from PIL import ImageDraw, Image
+from transformers.utils.constants import OPENAI_CLIP_MEAN, OPENAI_CLIP_STD
+
+import torch
+import numpy as np
+
+processor = Owlv2Processor.from_pretrained("google/owlv2-base-patch16-ensemble")
+model = Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble")
+
+# Cell 2
+image = Image.open("./back_can.jpg")
+image
+
+# Cell 3
+
+texts = [["a green sprite can"]]
+inputs = processor(text=texts, images=image, return_tensors="pt")
+for k, v in inputs.items():
+    print(k, v.shape)
+
+with torch.no_grad():
+    outputs = model(**inputs)
+
+# Cell 4
+
+
+def get_preprocessed_image(pixel_values):
+    pixel_values = pixel_values.squeeze().numpy()
+    unnormalized_image = (
+        pixel_values * np.array(OPENAI_CLIP_STD)[:, None, None]
+    ) + np.array(OPENAI_CLIP_MEAN)[:, None, None]
+    unnormalized_image = (unnormalized_image * 255).astype(np.uint8)
+    unnormalized_image = np.moveaxis(unnormalized_image, 0, -1)
+    unnormalized_image = Image.fromarray(unnormalized_image)
+    return unnormalized_image
+
+
+unnormalized_image = get_preprocessed_image(inputs.pixel_values)
+
+# Convert outputs (bounding boxes and class logits) to COCO API
+target_sizes = torch.Tensor([unnormalized_image.size[::-1]])
+results = processor.post_process_object_detection(
+    outputs=outputs, target_sizes=target_sizes, threshold=0.2
+)
+i = 0  # Retrieve predictions for the first image for the corresponding text queries
+text = texts[i]
+boxes, scores, labels = results[i]["boxes"], results[i]["scores"], results[i]["labels"]
+for box, score, label in zip(boxes, scores, labels):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+        f"Detected {text[label]} with confidence {round(score.item(), 3)} at location {box}"
+    )
+
+visualized_image = unnormalized_image.copy()
+
+draw = ImageDraw.Draw(visualized_image)
+
+index = np.argmax(scores).numpy()
+print(boxes[index])
+box = [round(i, 2) for i in boxes[index].tolist()]
+x1, y1, x2, y2 = tuple(box)
+draw.rectangle(xy=((x1, y1), (x2, y2)), outline="red")
+draw.text(xy=(x1, y1), text=text[labels[index]])
+
+
+# for box, score, label in zip(boxes, scores, labels):
+#     if score.numpy() < .5:
+#         continue
+#     box = [round(i, 2) for i in box.tolist()]
+#     x1, y1, x2, y2 = tuple(box)
+#     draw.rectangle(xy=((x1, y1), (x2, y2)), outline="red")
+#     draw.text(xy=(x1, y1), text=text[label])
+
+visualized_image
